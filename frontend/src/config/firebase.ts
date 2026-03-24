cf87a94435a3bed64995406b36a02e8fc7591e0c
Sahan Medhawa <dmsahanmedawa@gmail.com>
2026-03-25 00:37:33 +0530

    fix: .env imports

--- frontend/src/config/firebase.ts
+++ frontend/src/config/firebase.ts
@@ -9,13 +9,13 @@ import { GoogleAuthProvider } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDpOKjkbmnxXz-TLULZHG4F2HUbc-82hno",
-  authDomain: "healthmate-c2390.firebaseapp.com",
-  projectId: "healthmate-c2390",
-  storageBucket: "healthmate-c2390.firebasestorage.app",
-  messagingSenderId: "655160540915",
-  appId: "1:655160540915:web:9c5e4351a9cc8fd9c94186",
-  measurementId: "G-10LT1RW1GL"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
